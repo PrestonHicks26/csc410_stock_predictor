f7fb9db5d1bbadf74d49289eee5cc83cc93d5dc0
Preston L. Hicks <hicksp@rider.edu>
2022-03-23 20:49:54 -0400

    Update data_preparation.py
    changed labels to reflect calculation change to o-adjc

--- data_preparation.py
+++ data_preparation.py
@@ -36,7 +36,7 @@ def moving_average(i, window, df):
 def column_modification(name, df):
     company = []
     h_min_l = []
-    o_min_c = []
+    o_min_adjc = []
     _7_day_ma = []
     _14_day_ma = []
     _21_day_ma = []
@@ -44,7 +44,7 @@ def column_modification(name, df):
     for i in range(df.shape[0]):
         company.append(name)
         h_min_l.append(df.at[i, 'High'] - df.at[i, 'Low'])
-        o_min_c.append(df.at[i, 'Open'] - df.at[i, 'Adj Close'])
+        o_min_adjc.append(df.at[i, 'Open'] - df.at[i, 'Adj Close'])
         _7_day_ma.append(moving_average(i, 7, df))
         _14_day_ma.append(moving_average(i, 14, df))
         _21_day_ma.append(moving_average(i, 21, df))
@@ -55,7 +55,7 @@ def column_modification(name, df):
     df.drop('Close', axis=1, inplace=True)
     df.insert(0, 'Company', company)
     df['H-L'] = h_min_l
-    df['O-C'] = o_min_c
+    df['O-AdjC'] = o_min_adjc
     df['7 Day MA'] = _7_day_ma
     df['14 Day MA'] = _14_day_ma
     df['21 Day MA'] = _21_day_ma
